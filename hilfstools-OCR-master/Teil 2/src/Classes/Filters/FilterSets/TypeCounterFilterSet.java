f9f9b825ed88d5eed451732edc41fb537c822cd0
s344360 <ramadan.shweiki@stud-mail.uni-wuerzburg.de>
2021-05-25 15:04:56 +0200

    First commit

--- hilfstools-OCR-master/Teil 2/src/Classes/Filters/FilterSets/TypeCounterFilterSet.java
+++
@@ -1,9 +0,0 @@
-package Classes.Filters.FilterSets;
-
-import Classes.Data.DataLine;
-import Classes.Filters.FilterSet;
-
-public class TypeCounterFilterSet extends FilterSet<DataLine> {
-	public TypeCounterFilterSet() {
-	}
-}
